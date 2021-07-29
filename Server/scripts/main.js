7103e4c86eaf87fe12d1fcc47345df07848ba25a
Lorenzo <brainstew927@gmail.com>
2021-07-29 18:58:51 +0200

    added path variable

--- Server/scripts/main.js
+++ Server/scripts/main.js
@@ -6,19 +6,27 @@ const { exec } = require('child_process');
 
 var server = http.createServer(function(req, res) {
         res.end("");    
+    try{
+        let path = ".." + path.sep + "projects";
+        process.chdir(path);
+    }catch{
+        return -1;
+    }
     const queryObject = url.parse(req.url,true).query;
     if(queryObject["projectName"] != null){
         let prg = queryObject["projectName"];
         // creo il progetto
+        let path = "." + path.sep + prg + ".git";
         console.log(prg)
         // se non esiste la cartella creo la cartella con nome del progetto
-        if(!fs.existsSync(".." + path.sep + "projects"  +path.sep + prg + ".git")){
-            fs.mkdirSync(".." + path.sep + "projects"  +path.sep + prg + ".git");
+        if(!fs.existsSync(path)){
+            fs.mkdirSync(path);
             // dentro questa cartella creo il progetto git con init bare
             try {
-                process.chdir(".." + path.sep + "projects"  +path.sep + prg + ".git");
+                process.chdir(path);
                 exec("git init --bare");
                 res.writeHead(200)
+                process.chdir("..");
               }
               catch (err) {
                 console.log('chdir: ' + err);
